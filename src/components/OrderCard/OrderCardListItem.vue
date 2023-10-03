67934d3028500ff143cd2b1e3063a055fb9775d0
nurovic <mertnurovic@gmail.com>
2023-10-03 14:49:05 +0300

    order card components

--- src/components/OrderCard/OrderCardListItem.vue
+++ src/components/OrderCard/OrderCardListItem.vue
@@ -1,8 +1,26 @@
 <template lang="">
     <div
-    class="h-[125px]  bg-red-200 mb-4"
+    class="h-[125px] w-full flex border-2 rounded-[15px] mb-4"
     >
-        
+        <div class="w-1/5">
+            <img src="@/assets/s.jpg" class="object-cover w-full h-full rounded-[15px]" alt="">
+        </div>
+        <!--  -->
+        <div class="flex w-full justify-between px-4">
+            <div class='py-2 h-full flex flex-col justify-between'>
+            <div>Product Name</div>
+            <div>Adet: 2</div>
+        </div>
+        <!--  -->
+        <div class="py-2 h-full items-center flex flex-col justify-between">
+            <div>59.000.00 ₺</div>
+            <div class="flex border-2 rounded-full px-2 border-gray-200">
+                <div>-</div>
+                <div class="mx-5">2</div>
+                <div>+</div>
+            </div>
+        </div>
+        </div>
     </div>
 </template>
 <script setup>
--- src/components/OrderCard/OrderCardSummary.vue
+++ src/components/OrderCard/OrderCardSummary.vue
@@ -1,6 +1,12 @@
 <template lang="">
-    <div>
-        Summary
+    <div class="w-1/3 ml-2 border-2 border-gray-200 rounded-[15px] p-4 h-1/4">
+        <div class="font-[700]">Seçilen Ürünler(89)</div>
+        <div class="mt-4">
+            <div class="font-[700] text-gray-500">Genel Toplam: 22.000.00 ₺</div>
+        </div>
+        <div class="flex justify-center font-[800] h-12 rounded-[15px] items-center mt-12 w-full bg-orange-300 text-white ">
+            Alışverişi Tamamla
+        </div>
     </div>
 </template>
 <script>
